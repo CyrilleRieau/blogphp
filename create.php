1cabda05ca90ae939e83a5dad5ce0e44943bde01
Cyrille <cyrille.rieau@gmail.com>
2017-05-31 17:24:05 +0200

    Création index.php affichage liste fichiers

--- create.php
+++ create.php
@@ -1,4 +1,6 @@
 <?php
+echo '<a href="create.html" style ="text-align:right;">Formulaire </a><br/>';
+echo '<a href="index.php" style="text-align:center">Liste </a>';
 $title = $_POST['title'];
 $text = $_POST['text'];
 if (file_exists('directo')) {
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 echo '<h1 style ="text-align:center;">Mon Blog </h1>';
+echo '<a href="create.html" style ="text-align:right;">Formulaire </a>';
     echo '<ul>';
 $files= scandir('directo');
 foreach($files as $key => $value){
-    if ($value === '.txt' or $value ==='.' or $value ==='..'){
-    } else {
-        echo '<li><a href=directo/'.$value.'>'.$value.'</a></li>';
+    if (!is_file('directo/'.$value)){
+           echo '<li><a href=directo/'.$value.'>'.$value.'</a></li>';
     }
 }
 ;
