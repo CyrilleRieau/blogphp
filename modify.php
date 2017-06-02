21b18787c56bfe02b5e5f15b60f36d2c70786603
Cyrille <cyrille.rieau@gmail.com>
2017-06-02 09:56:32 +0200

    Modification de la zone de modification du texte

--- modify.php
+++ modify.php
@@ -1,7 +1,9 @@
 <?php
 $nomfile = $_GET['filemodif'];
+$content = file_get_contents('directo/'.$nomfile);
+
 echo '<form action="textmodif.php" method="GET">
-    <textarea type="text" name="textmodi"></textarea>
+    <textarea type="text" name="textmodi">'.$content.'</textarea>
     <input type="submit" value="Modifier" >
     <input type="hidden" name="modiftext" value="'.$nomfile.'"></form>';
     
--- textmodif.php
+++ textmodif.php
@@ -2,6 +2,6 @@
 echo '<a href="index.php" style="text-align:center">Liste </a>';
 $textmodi = $_GET['textmodi'];
 $fillemodi =$_GET['modiftext'];
-    file_put_contents('directo/'.$fillemodi, $textmodi, FILE_APPEND);
+    file_put_contents('directo/'.$fillemodi, $textmodi);
 
 ?>
